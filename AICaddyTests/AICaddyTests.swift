71f6827bbb43f5f2d86f21844b6c23b1c0fa0d41
Gehrig Rankin <rgrankin22@gmail.com>
2026-04-07 20:23:21 -0700

    Initial Commit

---
+++ AICaddyTests/AICaddyTests.swift
@@ -0,0 +1,16 @@
+//
+//  AICaddyTests.swift
+//  AICaddyTests
+//
+//  Created by Gehrig Rankin on 4/7/26.
+//
+
+import Testing
+
+struct AICaddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
